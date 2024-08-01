1df091315b1c4ccbac73ded9677dae78348cdd0a
xuan vuong <lou1svuong.dev@gmail.com>
2024-08-01 16:54:49 +0700

    feat: Add inventory page component

---
+++ app/dashboard/employees/page.tsx
@@ -0,0 +1,101 @@
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { IconEdit, IconEye, IconTrash } from "@tabler/icons-react";
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+];
+
+export default function EmployeesPage() {
+  return (
+    <Table className="bg-white rounded-md shadow-md">
+      <TableCaption>A list of your recent invoices.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[30%]">Name</TableHead>
+          <TableHead className="w-[20%]">Username</TableHead>
+          <TableHead className="w-[20%]">PhoneNumber</TableHead>
+          <TableHead className="w-[200px] ">Role</TableHead>
+          <TableHead className="text-center">Actions</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {invoices.map((invoice) => (
+          <TableRow key={invoice.invoice}>
+            <TableCell className="font-medium">{invoice.invoice}</TableCell>
+            <TableCell>{invoice.paymentStatus}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell>{invoice.paymentMethod}</TableCell>
+            <TableCell className="flex justify-center items-center gap-6">
+              <Button className="bg-[#05CD99]">
+                <IconEye />
+              </Button>
+              <Button className="bg-[#FFCE20]">
+                <IconEdit />
+              </Button>
+              <Button className="bg-[#EE5D50]">
+                <IconTrash />
+              </Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+      {/* <TableFooter>
+        <TableRow>
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
+        </TableRow>
+      </TableFooter> */}
+    </Table>
+  );
+}
