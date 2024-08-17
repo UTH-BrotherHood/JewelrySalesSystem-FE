95e368b0a40ac1a378578d9310808169084172aa
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-17 09:57:15 +0700

    chore: Update SignInPage layout to fix flex alignment issue

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,7 @@
 import SignInForm from "@/containers/auth-page/signInForm";
-
 export default function SignInPage() {
   return (
-    <div className="flex w-full h-screen items-center justify-center gap-8">
+    <div className=" flex w-full h-screen items-center justify-center gap-8">
       <div className="w-[40%]">
         <h1 className="text-5xl font-bold">Jewelry Sales System</h1>
         <p className="text-2xl">
