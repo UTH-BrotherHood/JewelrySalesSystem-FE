07fc63c7145b6929645fded03656e6b08cfc6239
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-17 09:59:51 +0700

    chore: Remove hero pattern background image from tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -73,9 +73,6 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      backgroundImage: {
-        "hero-pattern": "url('/assets/images/wallpaper-signin-page.jpg')",
-      },
     },
   },
   plugins: [require("tailwindcss-animate")],
