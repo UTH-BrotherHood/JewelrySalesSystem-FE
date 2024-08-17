2477583101cbbe5da5eb399f539f03d277858ad7
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-17 10:00:15 +0700

    feat: Add Top Products By Revenue chart to Dashboard page

---
+++ src/containers/dashboard/top-products-by-revenue.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { TrendingUp } from "lucide-react";
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis } from "recharts";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { month: "Jewelry 01", desktop: 186 },
+  { month: "Jewelry 02", desktop: 305 },
+  { month: "Jewelry 03", desktop: 237 },
+  { month: "Jewelry 04", desktop: 73 },
+  { month: "Jewelry 05", desktop: 209 },
+  { month: "Jewelry 06", desktop: 214 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+export function ProductsByRevenue() {
+  return (
+    <Card className="h-full rounded-lg w-[50%]">
+      <CardHeader>
+        <CardTitle>Products By Revenue Chart</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent className="mt-16">
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              top: 20,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="desktop" fill="var(--color-desktop)" radius={8}>
+              <LabelList
+                position="top"
+                offset={12}
+                className="fill-foreground"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm"></CardFooter>
+    </Card>
+  );
+}
