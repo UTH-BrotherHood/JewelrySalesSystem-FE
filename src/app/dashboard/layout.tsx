ba5b06f0323ab45c2cf1010ae96008cee332c0f4
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-31 04:21:50 +0700

    Refactor Dashboard page layout and components, and optimize rendering performance

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,16 +8,13 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div
-      className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-screen bg-[#F4F7FE]"
-      )}
-    >
-      <SidebarDashBoard></SidebarDashBoard>
-      <div className="w-full h-full px-10 py-14 overflow-y-scroll">
-        {children}
+    <>
+      <div className="rounded-md flex bg-[#F4F7FE] dark:bg-neutral-800 w-full h-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden">
+        <SidebarDashBoard></SidebarDashBoard>
+        <div className="ml-20 w-full px-10 py-14 overflow-y-auto">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
