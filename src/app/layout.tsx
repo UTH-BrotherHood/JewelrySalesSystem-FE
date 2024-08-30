66e28b2aa057f3659942aba7c702654f3f85e182
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-31 04:21:44 +0700

    feat: Add TopLoader component to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
+import TopLoader from "@/components/topLoader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <TopLoader />
           <Toaster />
           {children}
         </ThemeProvider>
