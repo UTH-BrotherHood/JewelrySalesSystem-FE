e400c7f1c6a1c0a5f28bfa68bc85772b716d6fc5
lou1s vuong <lou1svuong.dev@gmail.com>
2024-08-31 04:22:54 +0700

    Refactor data table toolbar to filter products by name instead of title

--- src/containers/dashboard/data/data.tsx
+++ src/containers/dashboard/data/data.tsx
@@ -9,63 +9,55 @@ import {
   StopwatchIcon,
 } from "@radix-ui/react-icons";
 
-export const labels = [
+export const categoryNames = [
   {
-    value: "bug",
-    label: "Bug",
-  },
-  {
-    value: "feature",
-    label: "Feature",
-  },
-  {
-    value: "documentation",
-    label: "Documentation",
+    value: "categoryName",
+    label: "hehe",
   },
 ];
 
-export const statuses = [
-  {
-    value: "backlog",
-    label: "Backlog",
-    icon: QuestionMarkCircledIcon,
-  },
-  {
-    value: "todo",
-    label: "Todo",
-    icon: CircleIcon,
-  },
-  {
-    value: "in progress",
-    label: "In Progress",
-    icon: StopwatchIcon,
-  },
-  {
-    value: "done",
-    label: "Done",
-    icon: CheckCircledIcon,
-  },
-  {
-    value: "canceled",
-    label: "Canceled",
-    icon: CrossCircledIcon,
-  },
-];
+// export const statuses = [
+//   {
+//     value: "backlog",
+//     label: "Backlog",
+//     icon: QuestionMarkCircledIcon,
+//   },
+//   {
+//     value: "todo",
+//     label: "Todo",
+//     icon: CircleIcon,
+//   },
+//   {
+//     value: "in progress",
+//     label: "In Progress",
+//     icon: StopwatchIcon,
+//   },
+//   {
+//     value: "done",
+//     label: "Done",
+//     icon: CheckCircledIcon,
+//   },
+//   {
+//     value: "canceled",
+//     label: "Canceled",
+//     icon: CrossCircledIcon,
+//   },
+// ];
 
-export const priorities = [
-  {
-    label: "Low",
-    value: "low",
-    icon: ArrowDownIcon,
-  },
-  {
-    label: "Medium",
-    value: "medium",
-    icon: ArrowRightIcon,
-  },
-  {
-    label: "High",
-    value: "high",
-    icon: ArrowUpIcon,
-  },
-];
+// export const priorities = [
+//   {
+//     label: "Low",
+//     value: "low",
+//     icon: ArrowDownIcon,
+//   },
+//   {
+//     label: "Medium",
+//     value: "medium",
+//     icon: ArrowRightIcon,
+//   },
+//   {
+//     label: "High",
+//     value: "high",
+//     icon: ArrowUpIcon,
+//   },
+// ];
