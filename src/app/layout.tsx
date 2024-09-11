d5d75f9f2aedce4a04c510dd737dac27a96e56d3
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-12 00:39:26 +0700

    Refactor layout.tsx: Update font imports and theme settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import type { Metadata } from "next";
 import Head from "next/head";
 import { Inter } from "next/font/google";
@@ -25,27 +26,26 @@ export default function RootLayout({
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body className={`${inter.className} font-roboto `}>
         <ThemeProvider
           attribute="class"
-          // defaultTheme="system"
-          // enableSystem
-          
+          defaultTheme="system"
+          enableSystem
           disableTransitionOnChange
-          
-          
         >
           <TopLoader />
           <Toaster />
           <StoreProvider>
-          <EmployeeProvider>
-
-            {children}
-       
-            </EmployeeProvider>
+            <EmployeeProvider>{children}</EmployeeProvider>
           </StoreProvider>
         </ThemeProvider>
       </body>
