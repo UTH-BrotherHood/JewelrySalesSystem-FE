7191a3500f62ca2e94b29004512cf5396de12c9b
lou1s vuong <lou1svuong.dev@gmail.com>
2024-09-12 00:39:33 +0700

    Refactor ProductsPage component: Fetch and display products from API

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,43 +1,106 @@
-import { promises as fs } from "fs";
-import path from "path";
-import { Metadata } from "next";
-import { z } from "zod";
+"use client";
 
+import { useCallback, useEffect, useState } from "react";
 import { columns } from "@/containers/dashboard/columns";
 import { DataTable } from "@/containers/dashboard/data-table";
-import { productSchema } from "@/containers/dashboard/data/productsSchema";
+import http from "@/utils/http";
+import Cookies from "js-cookie";
+import DefautlProductImg from "@/assets/image/default.jpg";
+import { useToast } from "@/components/ui/use-toast";
 
-export const metadata: Metadata = {
-  title: "Products | Dashboard",
-  description: "A task and issue tracker build using Tanstack Table.",
-};
-
-// Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), "src/containers/dashboard/data/products.json")
-  );
-
-  const products = JSON.parse(data.toString());
-
-  return z.array(productSchema).parse(products);
+interface Product {
+  productId: string;
+  categoryId: string;
+  name: string;
+  description: string;
+  costPrice: number;
+  weight: number;
+  laborCost: number;
+  imageUrl: string;
+  stoneCost: number;
+  stockQuantity: number;
+  createdAt: string;
+  updatedAt: string;
 }
 
-export default async function ProductsPage() {
-  const products = await getTasks();
+interface ApiResponse {
+  code: number;
+  message: string;
+  result: {
+    content: Product[];
+    pageable: {
+      pageNumber: number;
+      pageSize: number;
+    };
+    totalElements: number;
+    totalPages: number;
+  };
+}
+
+export default function ProductsPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const { toast } = useToast();
+
+  const fetchProducts = useCallback(async () => {
+    try {
+      const token = Cookies.get("token");
+      const response = await http.get<ApiResponse>(
+        "/products?page=0&size=1000",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      );
+      setProducts(response.data.result.content);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching products:", error);
+      setError("Không thể tải danh sách sản phẩm. Vui lòng thử lại sau.");
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  const handleProductDeleted = useCallback(
+    (deletedProductId: string) => {
+      setProducts((prevProducts) =>
+        prevProducts.filter((product) => product.productId !== deletedProductId)
+      );
+      toast({ description: "Product deleted successfully" });
+    },
+    [toast]
+  );
+
+  if (loading) {
+    return <div>Đang tải...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <>
       <div className="p-4 bg-white rounded-lg">
         <div className="flex items-center justify-between space-y-2">
           <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-            <p className="text-muted-foreground">
-              Here&apos;s a list of your products
-            </p>
+            <h2 className="text-2xl font-bold tracking-tight mb-4">Products</h2>
           </div>
         </div>
-        <DataTable data={products} columns={columns} />
+        <DataTable
+          data={products.map((product) => ({
+            ...product,
+            categoryName: product.categoryId,
+            // Đảm bảo imageUrl tồn tại, nếu không thì sử dụng một hình ảnh mặc định
+            imageUrl:
+              product.imageUrl || (DefautlProductImg as unknown as string),
+          }))}
+          columns={columns}
+        />
       </div>
     </>
   );
